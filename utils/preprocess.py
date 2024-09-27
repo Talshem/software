0ba8e47ce79dd33e01077a22d3e4b561a7a50005
EhrlichNati <128493983+EhrlichNati@users.noreply.github.com>
2024-09-27 11:56:37 +0300

    Add files via upload
    validation and preprocessing helpers

---
+++ utils/preprocess.py
@@ -0,0 +1,103 @@
+import itertools
+import pickle
+from datetime import datetime
+from multiprocessing import Pool
+from random import random
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from Bio.SeqUtils import gc_fraction as GC
+import sys
+from Bio import pairwise2, SeqIO
+from Bio.pairwise2 import format_alignment
+import numpy as np
+import json
+import Levenshtein as lev
+from fuzzysearch import find_near_matches
+import re
+from Bio import Align
+from Bio.pairwise2 import format_alignment
+import os
+from utils.suffix_tree import Suffixtree
+
+
+def find_similar_sequences_bio(trigger: str, sequences_dict: dict[str:str]) -> Align.Alignment:
+    """"function to find similar sequences to a trigger sequence
+    :param trigger: trigger sequence
+    :param sequences_dict: dictionary of sequences
+    :return: alignment of the trigger sequence and the most similar sequence
+    """""
+
+    aligner = Align.PairwiseAligner()
+    aligner.mode = 'local'
+    aligner.match_score = 2
+    aligner.mismatch_score = -1
+
+    alignments = aligner.align(trigger, sequences_dict)
+    return alignments[0]
+
+def chunks(data_iter, size):
+    """"function to split data into chunks for processing
+    :param data_iter: data to be split
+    :param size: size of the chunk
+    :return: chunked data
+    """""
+    it = iter(data_iter)
+    for i in range(0, len(data_iter), size):
+        yield {k: data_iter[k] for k in itertools.islice(it, size)}
+
+
+def run_chunks(data_path: str, func, args):
+    """""function to run large data sets in chunks using multiprocessing
+    :param data_path: path to the data file
+    :param func: function to be applied to the data
+    :param args: arguments to the function
+    :return: None
+    """""
+    file_path = data_path
+    with open(file_path, 'rb') as file:
+        file_data = pickle.load(file)
+
+    # Create enumeration for the data for reproducibility and file data indicator
+    current_time = datetime.now().strftime("%H:%M:%S")
+    enumerated_data = {key: i for i, key in enumerate(file_data.keys())}
+    with open(f'enumerated_data_{current_time}.pkl', 'wb') as f:
+        pickle.dump(enumerated_data, f)
+
+    # Process the data in chunks
+    n_process = 5
+    for i, chunk in enumerate(chunks(file_data, n_process)):
+        with Pool(n_process) as pool:
+            args = []
+            res = pool.map(func, args)
+
+        # Combine the results and save of to intermediate file
+        current_time = datetime.now().strftime("%H:%M:%S")
+        if res:
+            with open(f'intermediate_results_[{i * n_process}, {(i + 1) * n_process})_{current_time}.pkl',
+                      'wb') as f:
+                pickle.dump(res, f)
+
+
+def find_with_suffix_tree(trigg: str, data_dict: dict[str:str]) -> dict[str:str]:
+    """"function to find matches using suffix tree
+    :param trigg: trigger sequence
+    :param data_dict: dictionary of sequences key=gene name/rna name, value= rna/dna sequence
+    :return: dictionary of sequences that match the trigger sequence
+    """""
+    sub_seqs_dict = {}
+    for seq_name, seq in data_dict.items():
+        tree = Suffixtree(seq, seq_name)
+        try:
+            matches = tree.search(trigg)
+            for match in matches:
+                sub_seq = (seq[match:match + len(trigg)], match)
+                sub_seqs_dict[seq_name] = sub_seq
+        except Exception as e:
+            raise f"Error in finding matches for {seq_name} with error {e}"
+    return sub_seqs_dict
+
+
+if __name__ == '__main__':
+    pass
+
---
+++ utils/search_tests.py
@@ -0,0 +1,108 @@
+import pickle
+import random
+import time
+import numpy as np
+import pickle
+from seaborn import lineplot
+from tool.generate import find_homology
+from itertools import combinations
+import matplotlib.pyplot as plt
+from tool.generate import find_near_matches, build_homology_map
+
+
+def homo_sapiens_search_plot(edit_distance, trigger_len=23):
+    path = 'data/Homo_sapiens_GRCh38.pkl'
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    start = time.time()
+
+    for i in range(10):
+        trigger = ''.join([random.choice(['A', 'C', 'G', 'T']) for _ in range(trigger_len)])
+        matches_over_genes = {}
+        for dict in data:
+            gene = dict['gene']
+            transcript = dict['sequence']
+            res = build_homology_map(trigger, transcript, gene, edit_distance)
+            if res:
+                matches_over_genes[gene] = res
+    end = time.time()
+    tot = end - start
+    print(f" T={tot}")
+
+    """
+    plt.title('Transcriptome search time for different trigger lengths using fuzzy search')
+    plt.xlabel('Length of trigger')
+    plt.ylabel('Time in seconds')
+    plt.show()
+    """
+
+def gen_mutations(trigger: str, sub: int):
+    trig_len = len(trigger)
+    indexes_permutation = list(combinations(range(trig_len), sub))
+    nucleotides = ['A', 'C', 'G', 'T']
+    mutated_triggers = {}
+    for mutated_index in indexes_permutation:
+        trig_char_list = list(trigger)
+        for nuc_idx in mutated_index:
+            choices = nucleotides.copy()
+            nuc = trig_char_list[nuc_idx]
+            choices.remove(nuc)
+            trig_char_list[nuc_idx] = random.choice(choices)
+        mutated_trigger = ''.join(trig_char_list)
+        mutated_triggers[mutated_trigger] = mutated_index
+    return mutated_triggers
+
+def construct_dummy_seq(trigger_muts):
+    trig_n = len(trigger_muts)
+    n_list = random.sample(range(1, trig_n * 10), trig_n)
+    fillers = []
+
+    for x in n_list:
+        filler_seq = ''.join(random.choice(['A', 'C', 'G', 'T']) for _ in range(x))
+        fillers.append(filler_seq)
+
+    dummy_seq = ''
+    mut_trigger_mapping = {}
+    for trig, mut_index in trigger_muts.items():
+        dummy_seq += fillers.pop(0)
+        start_index = len(dummy_seq)
+        dummy_seq += trig
+        end_index = len(dummy_seq)
+        mut_trigger_mapping[trig] = (start_index, end_index)
+
+    return dummy_seq, mut_trigger_mapping
+
+def test_fuzzy_search(trig_len: int, sub: int):
+    trigger = ''.join([random.choice(['A', 'C', 'G', 'T']) for _ in range(trig_len)])
+    mutated_triggers = gen_mutations(trigger, sub)
+    test_seq, trig_muts = construct_dummy_seq(mutated_triggers)
+    test = find_near_matches(trigger, test_seq, max_substitutions=sub, max_deletions=0, max_insertions=0, max_l_dist=sub)
+    test_res = {}
+    for match in test:
+        start = match.start
+        end = match.end
+        sub_seq = test_seq[start:end]
+        test_res[sub_seq] = (start, end)
+
+    return test_res, trig_muts, test_seq
+
+
+if __name__ == "__main__":
+    # Search test validation
+
+    """
+    random.seed(42)
+    substitutions = 5
+    triggers_len = 25
+    res_array = []
+    for trig_len in range(15, triggers_len, 2):
+        for substitutions in range(5):
+            test_res, mutations_mapping, dummy_seq = test_fuzzy_search(trig_len=trig_len, sub=substitutions)
+            for mutated_trigger, mut_loc in mutations_mapping.items():
+                for search_res_trigger, res_loc in test_res.items():
+                    if mut_loc[0] == res_loc[0] and mut_loc[1] == res_loc[1]:
+                        mutations_mapping[mutated_trigger] = all(np.array(list(mutated_trigger)) == np.array(list(search_res_trigger)))
+            print(f'for trigger len={trig_len} with {substitutions} subs, all possible mutated triggers found: {all(mutations_mapping.values())}')
+            res_array.append(f'for trigger len={trig_len} with {substitutions} subs, all possible mutated triggers found: {all(mutations_mapping.values())}')
+    """
+
