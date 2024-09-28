e815067f0f0015f554d11ce15d4670dcd12f3459
EhrlichNati <128493983+EhrlichNati@users.noreply.github.com>
2024-09-28 11:24:24 +0300

    Update generate.py
    changes:
    -routing to google-cloud bucket 
    -complete pipeline with homo ranking only distance

--- tool/generate.py
+++ tool/generate.py
@@ -1,33 +1,29 @@
-import pickle
+
 import numpy as np
 import pandas as pd
 from fuzzysearch import find_near_matches
 from joblib import Parallel, delayed
-# from tool.server import bucket
+from server import bucket
 from tool.switch_generator import SwitchGenerator
+from tool.prokaryotic_switch_generator import ProkaryoticSwitchGenerator
 from tool.window_folding_based_selection import get_gene_top_ranked_windows, get_mRNA_opening_mfe
 from utils.send_mail import send_email_with_attachment as send
 from utils.seqeunce_consts import GFP_GENE
 
+
 EDIT_DIST = 5
-BATCH = 2
-N_HOMO = 3
-E_COLI_DATA_PATH = "/Users/netanelerlich/PycharmProjects/webTool/data/Escherichia_coli_ASM886v2.pkl"
-HUMAN_DATA_PATH = "/Users/netanelerlich/PycharmProjects/webTool/data/Homo_sapiens_GRCh38.pkl"
-YEAST_DATA_PATH = "/Users/netanelerlich/PycharmProjects/webTool/data/Saccharomyces_cerevisiae_S288C.pkl"
+TRIGGERS_BATCH = 2
+SWITCH_BATCH = 3
+
+E_COLI_DATA_PATH = "Escherichia_coli_ASM886v2.pkl"
+HUMAN_DATA_PATH = "Homo_sapiens_GRCh38.pkl"
+YEAST_DATA_PATH = "Saccharomyces_cerevisiae_S288C.pkl"
 
 DATA_PATHS = {
     "Prokaryote": E_COLI_DATA_PATH,
     "Homo sapiens": HUMAN_DATA_PATH,
     "Eukaryote": YEAST_DATA_PATH
 }
-
-
-def thread_execute(func, func_args, triggers, res, key):
-    results = Parallel(n_jobs=-1)(delayed(func)(trigger, *func_args) for trigger in triggers)
-    res[key] = results
-    return
-
 def extract_top_homology_sequences(triggers_homology_mapping):
     homo_dfs = []
     for trigger_homology in triggers_homology_mapping:
@@ -35,8 +31,13 @@ def extract_top_homology_sequences(triggers_homology_mapping):
         for gene_homology in trigger_homology:
             trigger_all_matches.extend(gene_homology.values())
         # TODO: optional- take only top closest distance
-        top_dist = sorted(trigger_all_matches[0], key=lambda single_homo_map: single_homo_map['distance'])
-        homo_dfs.append(pd.DataFrame(top_dist))
+
+        if trigger_all_matches:
+            top_dist_df = pd.DataFrame(
+                sorted(trigger_all_matches[0], key=lambda single_homo_map: single_homo_map['distance']))
+        else:
+            top_dist_df = pd.DataFrame(columns=['distance', 'idx', 'sequence', 'gene'])
+        homo_dfs.append(top_dist_df)
 
     # TODO: add expression levels
     # TODO: add opening mfe of trigger homology
@@ -46,39 +47,44 @@ def extract_top_homology_sequences(triggers_homology_mapping):
     # lower is better - distance= lower = more similar
     lower = ['distance']
     rrf_rank = [RRF(trigger_homology_df, higher, lower, index='sequence') for trigger_homology_df in homo_dfs]
-    homology_seqs = [list(ranked_df['sequence'][:min(N_HOMO, len(ranked_df.index))]) for ranked_df in rrf_rank]
-
-    return homology_seqs, rrf_rank
-
+    return rrf_rank
 
 def rout_input(email, target_seq, trigger, reporter_gene, cell_type, user_trigger_boo, transcripts_dict):
-
     results = pd.DataFrame()
-    data_path = DATA_PATHS[cell_type]
-    with open(data_path, 'rb') as f:
-        cell_type_transcripts = pickle.load(f)
+    blob_name = DATA_PATHS[cell_type]
+
+    blob = bucket.blob(blob_name)
+    cell_type_transcripts = blob.download_as_bytes()
+
+    #with open(data_path, 'rb') as f:
+    # cell_type_transcripts = pickle.load(f)
+
 
     if transcripts_dict != 'EMPTY':
         transcripts_dict = cell_type_transcripts | transcripts_dict
+    else:
+        transcripts_dict = cell_type_transcripts
+
     # TODO: calc time for BATCH
     triggers_with_mfe = np.array([[trigger, 1]])
     if user_trigger_boo == 'EMPTY':
         optional_triggers_df = get_gene_top_ranked_windows(target_seq)
-        n_triggers = min(BATCH, len(optional_triggers_df.index))
+        n_triggers = min(TRIGGERS_BATCH, len(optional_triggers_df.index))
         triggers_with_mfe = np.array(optional_triggers_df.loc[:n_triggers, ["window_sequence", "mfe_score"]])
+    results[['trigger_window', 'mfe_score']] = triggers_with_mfe
 
     triggers_seqs = triggers_with_mfe[:, 0]
-    results[['trigger_window', 'mfe_score']] = triggers_with_mfe
-    homo_res = Parallel(n_jobs=-1)(delayed(find_homology())(trigger, transcripts_dict) for trigger in triggers_seqs)
 
-    homology_sequences, rrf_ranks = extract_top_homology_sequences(homo_res)
+    homo_res = Parallel(n_jobs=-1)(delayed(find_homology)(trigger, transcripts_dict) for trigger in triggers_seqs)
+    rrf_ranks = extract_top_homology_sequences(homo_res)
+    homology_sequences = [ranked_df['sequence'][0] for ranked_df in rrf_ranks]
 
     # TODO: organize input parameters after Peleg's changes
-    switch_res = Parallel(n_jobs=N_HOMO)(
-        delayed(generate_switch())(trigger, homo_seqs_list, reporter_gene, cell_type) for trigger, homo_seqs_list in zip(triggers_seqs, homology_sequences))
-    results['switch'] = switch_res
-
-    prepare_and_send_report(results,rrf_ranks,email)
+    switch_res = Parallel(n_jobs=SWITCH_BATCH)(
+        delayed(generate_switch)(trigger, top_homology_sequence, reporter_gene, cell_type) for trigger, top_homology_sequence in zip(triggers_seqs[:SWITCH_BATCH], homology_sequences[:SWITCH_BATCH]))
+    results[['switch', "ensemble_defect", "complex_concentration"]] = (
+        pd.DataFrame(switch_res, columns=['switch', "ensemble_defect", "complex_concentration"]))
+    prepare_and_send_report(results, rrf_ranks, email)
     return
 
 
@@ -100,7 +106,6 @@ def RRF(ranking_df, higher_is_better_cols, lower_is_better_cols, index, k=60):
 
     return ranking_df.sort_values(by=f'{index}_RRF', ascending=True)
 
-
 def build_homology_map(trigger, seq, gene_name):
     sequence_match_mapping = []
     matches = find_near_matches(trigger, seq, max_insertions=0, max_deletions=0, max_l_dist=EDIT_DIST)
@@ -113,11 +118,11 @@ def build_homology_map(trigger, seq, gene_name):
         sequence_match_mapping.append({'distance': distance, 'idx': seq_location, 'sequence': sub_seq, 'gene': gene_name})
     return sequence_match_mapping
 
-
 def find_homology(sequence, genome_data):
     genes_sub_sequences = []
     for gene_data_dict in genome_data:
         gene_sub_seqs = {}
+
         gene_name = gene_data_dict['gene']
         mRNA_seq = gene_data_dict['sequence']
         seq_match_mapping = build_homology_map(sequence, mRNA_seq, gene_name)
@@ -126,31 +131,35 @@ def find_homology(sequence, genome_data):
             genes_sub_sequences.append(gene_sub_seqs)
     return genes_sub_sequences
 
-
-def generate_switch(trigger, homology_seqs, reporter_gene, cell_type):
+def generate_switch(trigger, homologous_sequence, reporter_gene, cell_type):
     # blob = bucket.blob(cell_type)
     # content = blob.download_as_text()
-    # TODO:NO ORGANISM CONSIDERATION
-    switch_generator = SwitchGenerator(translated_gene_sequence=reporter_gene)
+    if cell_type == "Prokaryote":
+        switch_generator = ProkaryoticSwitchGenerator(translated_gene_sequence=reporter_gene)
+    else:
+        switch_generator = SwitchGenerator(translated_gene_sequence=reporter_gene)
     switch_designed_strand, ensemble_defect, complex_concentration, switch_mfe_structure = switch_generator.get_switch(
-        trigger_sequence=trigger, healthy_sequence=homology_seqs)
-    return switch_designed_strand, complex_concentration, switch_mfe_structure
+        trigger_sequence=trigger, healthy_sequence=homologous_sequence)
 
+    return switch_designed_strand, ensemble_defect,complex_concentration
 
 def prepare_and_send_report(df_results, rrf_ranks, email):
+    #print(df_results)
+    #print(rrf_ranks)
+    #print(email)
     combined_rows = []
     rrf_ranks = rrf_ranks.copy()
     for index, row in df_results.iterrows():
         combined_df = pd.concat([pd.DataFrame([row]), rrf_ranks[index]], ignore_index=True)
         combined_rows.append(combined_df)
     final_df = pd.concat(combined_rows, ignore_index=True)
+    print(final_df)
     send(final_df, email)
     return
 
 
 if __name__ == '__main__':
     # Get the arguments from the user form.
-    """
     s_mail = sys.argv[1]
     s_target_seq = sys.argv[2]
     s_trigger = sys.argv[3]
@@ -158,34 +167,7 @@ if __name__ == '__main__':
     s_cell_type = sys.argv[5]
     s_user_trigger_boo = sys.argv[6]
     s_transcripts_dict = sys.argv[7]
-    print(s_mail, s_target_seq, s_trigger, s_reporter_gene, s_cell_type, s_user_trigger_boo, s_transcripts_dict)
+    print('args sent to route input- pipeline exe function')
     rout_input(s_mail, s_target_seq, s_trigger, s_reporter_gene, s_cell_type, s_user_trigger_boo, s_transcripts_dict)
-    """
-
-    """
-    import sys
-
-    sys.path.append("/Users/netanelerlich/anaconda3.7/anaconda3/pkgs/viennarna-2.5.1-py310pl5321h3899b80_0/lib/python3.10/site-packages/RNA")
-    from ViennaRNA import RNA
-    sequence = "AUGCGGAUACGAGUAG"
-    fc = RNA.fold_compound(sequence)
-    mfe_structure, mfe = fc.mfe()
-
-    print(f"Sequence: {sequence}")
-    print(f"Predicted Structure: {mfe_structure}")
-    print(f"MFE: {mfe} kcal/mol")
-    """
 
 
-    # 'gene': 'QCR2', 'protein': 'ubiquinol--cytochrome-c reductase subunit 2
-    mrna = "TAGATTGCAATTTGCCCAGGGGTCAGTTAGAAGGTTGACCGTTTCCGCTAGAGACGCACCTACTAAAATATCTACATTGGCTGTTAAGGTCCATGGAGGGTCTCGTTATGCAACCAAGGATGGTGTGGCCCATCTTTTAAACAGATTCAACTTTCAAAACACGAACACTAGATCAGCTTTGAAATTAGTCAGAGAATCCGAATTATTAGGGGGAACTTTTAAGTCTACCTTGGATAGGGAATACATCACTTTGAAAGCTACCTTTT"
-    cell_type = "Eukaryote"
-    s_mail = 'erlichnet57@gmail.com'
-    s_target_seq = mrna
-    s_trigger = 'EMPTY'
-    s_reporter_gene = GFP_GENE
-    s_cell_type = cell_type
-    s_user_trigger_boo = 'EMPTY'
-    s_transcripts_dict = 'EMPTY'
-    rout_input(s_mail, s_target_seq, s_trigger, s_reporter_gene, s_cell_type, s_user_trigger_boo, s_transcripts_dict)
-    print("done")
